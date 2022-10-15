cd5cee70c1134b0442baf1da81d375a0bd9b3083
Larissa Perinoto <98956659+larissaperinoto@users.noreply.github.com>
2022-10-15 11:24:52 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,12 +7,4 @@ const { productsRoutes, salesRoutes } = require('./routes');
 app.use('/products', productsRoutes);
 app.use('/sales', salesRoutes);
 
-// não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
-
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
